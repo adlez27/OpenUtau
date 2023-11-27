39e7925a47b03d24f71805c12262a41ca1e40b67
Mark Beltran <markmbeltran@gmail.com>
2023-11-27 16:25:00 +0800

    Set up dict replacements

--- AbsideePhonemizer.Test/AbcEnTest.cs
+++ AbsideePhonemizer.Test/AbcEnTest.cs
@@ -12,16 +12,54 @@ namespace AbsideePhonemizer.Test {
             return new AbsideeEnglishPhonemizer();
         }
 
-        protected override Voicebank GetVoicebank(string singerName) {
+        protected override Voicebank GetVoicebank(string _) {
             return AbsideePhonemizerUtil.GetVoicebank();
         }
 
+        private void SameAltsTonesColorsTest(string[] lyrics, string[] aliases) {
+            SameAltsTonesColorsTest("", lyrics, aliases.Select(a => $"{a}_G3").ToArray(), "", "C4", "");
+        }
+
         [Theory]
-        [InlineData(
-            new string[] { },
-            new string[] { })]
-        public void BasicPhonemizeTest(string[] lyrics, string[] aliases) {
-            SameAltsTonesColorsTest("", lyrics, aliases, "", "C4", "");
+        // Rewrite this later
+        [InlineData("want",
+            new string[] { "() w a", "(a) n t" })]
+        [InlineData("at",
+            new string[] { "()  e", "(e) t" })]
+        [InlineData("a",
+            new string[] { "()  @", "(@) " })]
+        [InlineData("aw",
+            new string[] { "()  o", "(o) " })]
+        [InlineData("eh",
+            new string[] { "()  e", "(e) " })]
+        [InlineData("er",
+            new string[] { "()  @", "(@) " })]
+        [InlineData("f",
+            new string[] { "()  e", "(e) ff" })]
+        [InlineData("he",
+            new string[] { "() h i", "(i) " })]
+        [InlineData("it",
+            new string[] { "()  e", "(e) t" })]
+        [InlineData("ee",
+            new string[] { "()  i", "(i) " })]
+        [InlineData("j",
+            new string[] { "() j e", "(e) y" })]
+        [InlineData("would",
+            new string[] { "() w @", "(@) d" })]
+        [InlineData("uw",
+            new string[] { "()  u", "(u) " })]
+        [InlineData("eye",
+            new string[] { "()  a", "(a) y" })]
+        [InlineData("ey",
+            new string[] { "()  e", "(e) y" })]
+        [InlineData("oy",
+            new string[] { "()  o", "(o) y" })]
+        [InlineData("oh",
+            new string[] { "()  o", "(o) w" })]
+        [InlineData("ouch",
+            new string[] { "()  a", "(a) w ch" })]
+        public void RemapPhonemesTest(string lyric, string[] aliases) {
+            SameAltsTonesColorsTest("", new string[] { lyric }, aliases, "", "C4", "");
         }
     }
 }
--- AbsideePhonemizer/AbsideeEnglishPhonemizer.cs
+++ AbsideePhonemizer/AbsideeEnglishPhonemizer.cs
@@ -2,22 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using OpenUtau.Api;
+using OpenUtau.Core.G2p;
 using OpenUtau.Core.Ustx;
 using OpenUtau.Plugin.Builtin;
 
 namespace AbsideePhonemizer {
     [Phonemizer("Absidee English Phonemizer", "EN ABC", "Adlez27", "EN")]
     public class AbsideeEnglishPhonemizer : SyllableBasedPhonemizer {
-        protected override string[] GetVowels() {
-            throw new NotImplementedException();
+        protected override string[] GetVowels() =>
+            "a i u e o @ ay ey oy ow aw".Split();
+
+        protected override string[] GetConsonants() => new string[0];
+            //"k ky g gy ng s sh z j zh t ch ts d dz n ny h hy f ff b by p py m my y r ry l rr w v th dh".Split();
+
+        protected override string GetDictionaryName() => "";
+        protected override IG2p LoadBaseDictionary() => new ArpabetG2p();
+
+        protected override Dictionary<string, string> GetDictionaryPhonemesReplacement() =>
+            new Dictionary<string, string> {
+                { "aa", "a" },
+                { "ae", "e" },
+                { "ah", "@" },
+                { "ao", "o" },
+                { "eh", "e" },
+                { "er", "@" },
+                { "f", "ff" },
+                { "hh", "h" },
+                { "ih", "e" },
+                { "iy", "i" },
+                { "jh", "j" },
+                { "uh", "@" },
+                { "uw", "u" }
+            };
+
+        protected override string[] GetSymbols(Note note) {
+            var symbols = base.GetSymbols(note);
+            var diphthongs = "ay ey oy ow aw".Split();
+            var adjustedSymbols = new List<string>();
+            foreach(var symbol in symbols) {
+                if (diphthongs.Contains(symbol)) {
+                    adjustedSymbols.Add(symbol.First().ToString());
+                    adjustedSymbols.Add(symbol.Last().ToString());
+                } else {
+                    adjustedSymbols.Add(symbol);
+                }
+            }
+            
+            return adjustedSymbols.ToArray();
         }
 
         protected override List<string> ProcessEnding(Ending ending) {
-            throw new NotImplementedException();
+            return new List<string> { ending.ToString() };
         }
 
         protected override List<string> ProcessSyllable(Syllable syllable) {
-            throw new NotImplementedException();
+            return new List<string> { syllable.ToString() };
         }
     }
 }
